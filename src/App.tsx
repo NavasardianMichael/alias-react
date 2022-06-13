a4d17d14622a3b540bc74d82bf6577bb141aaa78
NavasardianMichael <navasardianmichael@gmail.com>
2022-06-13 19:12:25 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Button, IconButton } from '@mui/material';
+import SettingsIcon from '@mui/icons-material/Settings';
+import logo from './assets/images/logo.png'; 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='upper-section'>
+        <IconButton color='primary' size='large'>
+          <SettingsIcon />
+        </IconButton>
+      </div>
+      <div className='middle-section'>
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className='lower-section'>
+        <Button variant="contained" size='large'>Play</Button>
+      </div>
     </div>
   );
 }
-
 export default App;
