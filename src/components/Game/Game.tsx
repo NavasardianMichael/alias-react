83f8ed3832d46938118750ee9f22b2e02a15e0e6
NavasardianMichael <navasardianmichael@gmail.com>
2022-06-29 00:20:43 +0400

    timer of game page is done

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,8 +1,31 @@
-import React, { FC } from 'react'
+import { FC, useCallback, useEffect, useRef, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { PAGES } from '../../constants/view'
+import { setCurrentPage } from '../../store/view/actionCreators'
 
 const Game: FC<{}> = () => {
+
+  const dispatch = useDispatch()
+  const [ timer, setTimer ] = useState(60)
+  const timerIntervalRef = useRef<NodeJS.Timer>()
+  
+  const handleTimer = useCallback(() => {
+    setTimer(prev => prev - 1)
+  }, [timer])
+
+  useEffect(() => {
+    timerIntervalRef.current = setInterval(handleTimer, 1000)
+    return () => clearInterval(timerIntervalRef.current)
+  }, [])
+
+  useEffect(() => {
+    if(timer) return
+    clearInterval(timerIntervalRef.current)
+    dispatch(setCurrentPage(PAGES.home)) 
+  }, [timer])
+
   return (
-    <h1>Game Screen</h1>
+    <p>{timer}</p>
   )
 }
 
